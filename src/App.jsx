9be99aaf1ecb24d564eb7f62892d9238e152acf9
Piyush Gupta <124340622+piyushgupta12345@users.noreply.github.com>
2024-03-03 23:54:43 +0530

    login setup

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import {
 } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
-import Signup from "./pages/register/Register";
+import Register from "./pages/register/Register";
 import AddNote from "./pages/addnote/AddNote";
 import UpdateNote from "./pages/updatenote/UpdateNote";
 // import NoPage from "./pages/nopage/NoPage";
@@ -14,16 +14,33 @@ import MyState from "./context/data/myState";
 import { Toaster } from 'react-hot-toast';
 
 function App() {
+  
   return (
     <MyState>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={
+            // <ProtectedRoute>
+              <Home />
+            // </ProtectedRoute>
+          } />
+          <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/addnote" element={<AddNote />} />
-          <Route path="/updatenote" element={<UpdateNote />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/addnote" element={
+            // <ProtectedRoute>
+              <AddNote />
+            // </ProtectedRoute>
+          } />
+          <Route path="/updatenote" element={
+            // <ProtectedRoute>
+              <UpdateNote />
+            // </ProtectedRoute> 
+          } />
+          <Route path="/profile" element={
+            // <ProtectedRoute>
+              <Profile />
+            // </ProtectedRoute> 
+          } />
           {/* <Route path="/*" element={<NoPage />} /> */}
         </Routes>
         <Toaster />
@@ -33,3 +50,12 @@ function App() {
 }
 
 export default App
+
+export const ProtectedRoute = ({ children }) => {
+  const token = localStorage.setItem('token')
+  if (token) {
+    return children
+  } else {
+    return <Navigate to={'/login'} />
+  }
+}
